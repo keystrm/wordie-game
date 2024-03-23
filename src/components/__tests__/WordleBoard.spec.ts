b48f7d0b1433d638bb4952dab3a5343f2796d787
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-23 17:31:22 +0530

    refactor for helper function submitguess

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -9,19 +9,18 @@ describe('WordleBoard', () => {
   beforeEach(() => {
     wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
   })
+  const playerSubmitsGuess = async (guess: string) => {
+    const guessInput = wrapper.find('input[type=text]')
+    await guessInput.setValue(guess)
+    await guessInput.trigger('keydown.enter')
+  }
 
   test('a success message appears when user guess the word of the day', async () => {
-    const guessInput = wrapper.find('input[type=text]')
-    await guessInput.setValue('Tests')
-    await guessInput.trigger('keydown.enter')
-
+    await playerSubmitsGuess('Tests')
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
   test('a failed message appears when user guess the word of the day incorrectly', async () => {
-    const guessInput = wrapper.find('input[type=text]')
-    await guessInput.setValue('Wrong')
-    await guessInput.trigger('keydown.enter')
-
+    await playerSubmitsGuess('Wrong')
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
   test('no end-message appears when user has not made a guess', async () => {
