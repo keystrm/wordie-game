316d40823acd20f5fe73261377b272bc5c38abb1
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-23 16:37:59 +0530

    extract const

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,17 +1,15 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import { VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
   it('a success message appears when user guess the word of the day', async () => {
-    //Arrange
     const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'Tests' } })
 
-    //Act
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue('Tests')
     await guessInput.trigger('keydown.enter')
 
-    //Assert
-    expect(wrapper.text()).toContain('You won!')
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 })
