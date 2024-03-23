ec7401f2973b2a13e280e7543fc0f311403dcc48
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-23 15:53:34 +0530

    rename component to WordleBoard

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import HelloWorld from '../WordleBoard.vue'
 
-describe('HelloWorld', () => {
+describe('WordleBoard', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
