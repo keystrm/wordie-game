10682785cac20efba9eda56d910b8ffcacc1185e
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-23 17:27:06 +0530

    extract to beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -3,9 +3,14 @@ import WordleBoard from '../WordleBoard.vue'
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
-  test('a success message appears when user guess the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'Tests' } })
+  let wordOfTheDay = 'Tests'
+  let wrapper: ReturnType<typeof mount>
 
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  })
+
+  test('a success message appears when user guess the word of the day', async () => {
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue('Tests')
     await guessInput.trigger('keydown.enter')
@@ -13,8 +18,6 @@ describe('WordleBoard', () => {
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
   test('a failed message appears when user guess the word of the day incorrectly', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'Tests' } })
-
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue('Wrong')
     await guessInput.trigger('keydown.enter')
@@ -22,8 +25,6 @@ describe('WordleBoard', () => {
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
   test('no end-message appears when user has not made a guess', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'Tests' } })
-
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
